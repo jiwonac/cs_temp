3e30d5f170b742187fd3681832d7252dd3437109
Jiwon Chang <me@jiwonc.net>
2023-06-29 12:58:33 +0900

    exp, transpose, relu impl

--- src/language.rs
+++ src/language.rs
@@ -3,11 +3,14 @@ use egg::*;
 define_language! {
     pub enum TnsrLang {
         "input"  = Input([Id; 1]), // Input T name@T/F@dim1_dim2_...
-        "ewadd"  = Ewadd([Id; 2]), // T x T --> T
-        "Mul"  = Mul([Id; 2]), // T x T --> T
+        "Add"    = Add([Id; 2]), // T x T --> T
+        "Mul"    = Mul([Id; 2]), // T x T --> T
         "smul"   = Smul([Id; 2]), // S x T --> T
         "MatMul" = MatMul([Id; 2]), // T x T --> T
         "noop"   = Noop([Id;2]), // No-op used to combine multiple outputs
+        "Relu"   = Relu([Id;1]), // Relu activation T --> T
+        "Exp"    = Exp([Id; 2]), // Elementwise power T x S --> T
+        "Transpose"  = Transpose([Id;1]), // T --> T
         Num(i32),
         Var(Symbol),
     }
@@ -88,7 +91,7 @@ impl Analysis<TnsrLang> for TnsrAnalysis {
                 }
             },
 
-            TnsrLang::Ewadd([a, b]) => {
+            TnsrLang::Add([a, b]) => {
                 assert!(x(a).dtype == DataKind::Tnsr);
                 assert!(x(b).dtype == DataKind::Tnsr);
                 assert!(&x(a).dims == &x(b).dims);
@@ -154,6 +157,40 @@ impl Analysis<TnsrLang> for TnsrAnalysis {
                 }
             },
 
+            TnsrLang::Relu([t]) => {
+                assert!(x(t).dtype == DataKind::Tnsr);
+                let foldable = x(t).constant_foldable;
+                Self::Data {
+                    dtype: DataKind::Tnsr,
+                    name: String::new(),
+                    dims: vec![],
+                    constant_foldable: foldable,
+                }
+            },
+
+            TnsrLang::Exp([t, s]) => {
+                assert!(x(t).dtype == DataKind::Tnsr);
+                assert!(x(s).dtype == DataKind::Tnsr);
+                let foldable = x(t).constant_foldable && x(s).constant_foldable;
+                Self::Data {
+                    dtype: DataKind::Tnsr,
+                    name: String::new(),
+                    dims: vec![],
+                    constant_foldable: foldable,
+                }
+            },
+
+            TnsrLang::Transpose([t]) => {
+                assert!(x(t).dtype == DataKind::Tnsr);
+                let foldable = x(t).constant_foldable;
+                Self::Data {
+                    dtype: DataKind::Tnsr,
+                    name: String::new(),
+                    dims: vec![],
+                    constant_foldable: foldable,
+                }
+            },
+
             TnsrLang::Num(_n) => Self::Data {
                 dtype: DataKind::Scalar,
                 name: String::new(),
@@ -172,26 +209,40 @@ impl Analysis<TnsrLang> for TnsrAnalysis {
 }
 
 pub fn rules<A: Analysis<TnsrLang>>() -> Vec<Rewrite<TnsrLang, A>> { vec![
-    rewrite!("ewadd-associative"; "(ewadd ?x (ewadd ?y ?z))" => "(ewadd (ewadd ?x ?y) ?z)"),
-    rewrite!("ewadd-commutative"; "(ewadd ?x ?y)" => "(ewadd ?y ?x)"),
+    rewrite!("ewadd-associative"; "(Add ?x (Add ?y ?z))" => "(Add (Add ?x ?y) ?z)"),
+    rewrite!("ewadd-commutative"; "(Add ?x ?y)" => "(Add ?y ?x)"),
     rewrite!("Mul-associative"; "(Mul ?x (Mul ?y ?z))" => "(Mul (Mul ?x ?y) ?z)"),
     rewrite!("Mul-commutative"; "(Mul ?x ?y)" => "(Mul ?y ?x)"),
-    rewrite!("distributivity-0"; "(Mul (ewadd ?x ?y) ?z)" => "(ewadd (Mul ?x ?z) (Mul ?y ?z))"),
+    rewrite!("distributivity-0"; "(Mul (Add ?x ?y) ?z)" => "(Add (Mul ?x ?z) (Mul ?y ?z))"),
     rewrite!("smul-associative"; "(smul (smul ?x ?y) ?w)" => "(smul ?x  (smul ?y ?w))"),
-    rewrite!("distributivity-1"; "(smul (ewadd ?x ?y) ?w)" => "(ewadd (smul ?x ?w)  (smul ?y ?w))"),
+    rewrite!("distributivity-1"; "(smul (Add ?x ?y) ?w)" => "(Add (smul ?x ?w)  (smul ?y ?w))"),
     rewrite!("operator-commutativity-0"; "(smul (Mul ?x ?y) ?w)" => "(Mul ?x  (smul ?y ?w))"),
     rewrite!("MatMul-is-associative"; "(MatMul ?x (MatMul ?y ?z))" => "(MatMul (MatMul ?x ?y) ?z)"),
     rewrite!("MatMul-is-linear-0"; "(smul (MatMul ?x ?y) ?w)" => "(MatMul ?x  (smul ?y ?w))"),
-    rewrite!("MatMul-is-linear-1"; "(MatMul ?x (ewadd ?y ?z))" => "(ewadd (MatMul ?x ?y) (MatMul ?x ?z))"),
-    rewrite!("-ewadd-associative"; "(ewadd (ewadd ?x ?y) ?z)" =>  "(ewadd ?x (ewadd ?y ?z))"),
+    rewrite!("MatMul-is-linear-1"; "(MatMul ?x (Add ?y ?z))" => "(Add (MatMul ?x ?y) (MatMul ?x ?z))"),
+    rewrite!("-ewadd-associative"; "(Add (Add ?x ?y) ?z)" =>  "(Add ?x (Add ?y ?z))"),
     rewrite!("-Mul-associative"; "(Mul (Mul ?x ?y) ?z)" => "(Mul ?x (Mul ?y ?z))"),
-    rewrite!("-distributivity-0"; "(ewadd (Mul ?x ?z) (Mul ?y ?z))" => "(Mul (ewadd ?x ?y) ?z)"),
+    rewrite!("-distributivity-0"; "(Add (Mul ?x ?z) (Mul ?y ?z))" => "(Mul (Add ?x ?y) ?z)"),
     rewrite!("-smul-associative"; "(smul ?x  (smul ?y ?w))" => "(smul (smul ?x ?y) ?w)"),
-    rewrite!("-distributivity-1"; "(ewadd (smul ?x ?w)  (smul ?y ?w))" => "(smul (ewadd ?x ?y) ?w)"),
+    rewrite!("-distributivity-1"; "(Add (smul ?x ?w)  (smul ?y ?w))" => "(smul (Add ?x ?y) ?w)"),
     rewrite!("-operator-commutativity-0"; "(Mul ?x  (smul ?y ?w))" => "(smul (Mul ?x ?y) ?w)"),
     rewrite!("-MatMul-is-associative"; "(MatMul (MatMul ?x ?y) ?z)" => "(MatMul ?x (MatMul ?y ?z))"),
     rewrite!("-MatMul-is-linear-0"; "(MatMul ?x  (smul ?y ?w))" => "(smul (MatMul ?x ?y) ?w)"),
-    rewrite!("-MatMul-is-linear-1"; "(ewadd (MatMul ?x ?y) (MatMul ?x ?z))" => "(MatMul ?x (ewadd ?y ?z))"),
+    rewrite!("-MatMul-is-linear-1"; "(Add (MatMul ?x ?y) (MatMul ?x ?z))" => "(MatMul ?x (Add ?y ?z))"),
+    rewrite!("relu-transpose"; "(relu (transpose ?x))" => "(transpose (relu ?x))"),
+    rewrite!("-relu-transpose"; "(transpose (relu ?x))" => "(relu (transpose ?x))"),
+    rewrite!("transpose-is-its-own-inverse"; "(transpose (transpose ?x))" => "?x"),
+    rewrite!("transpose-commutativity-ewadd"; "(transpose (Add ?x ?y))" => "(Add (transpose ?x)  (transpose ?y))"),
+    rewrite!("-transpose-commutativity-ewadd"; "(Add (transpose ?x)  (transpose ?y))" => "(transpose (Add ?x ?y))"),
+    rewrite!("transpose-commutativity-ewmul"; "(transpose (ewmul ?x ?y))" => "(ewmul (transpose ?x)  (transpose ?y))"),
+    rewrite!("transpose-commutativity-ewmul"; "(ewmul (transpose ?x)  (transpose ?y))" => "(transpose (ewmul ?x ?y))"),
+    rewrite!("transpose-commutativity-smul"; "(smul (transpose ?x) ?w)" => "(transpose (smul ?x ?w))"),
+    rewrite!("-transpose-commutativity-smul"; "(transpose (smul ?x ?w))" => "(smul (transpose ?x) ?w)"),
+    rewrite!("matmul-transpose"; "(transpose (matmul ?x ?y))" => "(matmul (transpose ?y)  (transpose ?x))"),
+    rewrite!("exp-ewmul"; "(Exp (Mul ?x ?y) ?z)" => "(Mul (Exp ?x ?z) (Exp ?y ?z))"),
+    rewrite!("-exp-ewmul"; "(Mul (Exp ?x ?z) (Exp ?y ?z))" => "(Exp (Mul ?x ?y) ?z)"),
+    rewrite!("exp-transpose"; "(Exp (Transpose ?x) ?y)" => "(Transpose (Exp ?x ?y))"),
+    rewrite!("-exp-transpose"; "(Transpose (Exp ?x ?y))" => "(Exp (Transpose ?x) ?y)"),
 ]}
 
 pub struct TnsrCost<'a> {
@@ -208,8 +259,11 @@ impl CostFunction<TnsrLang> for TnsrCost<'_> {
     }
 }
 
+const COPY_COST: f32 = 0.1;
 const ADDITION_COST: f32 = 1.0;
 const MULTIPLY_COST: f32 = 6.0;
+const EXP_COST: f32 = 10.0;
+const RELU_COST: f32 = 1.0;
 
 fn get_op_cost(egraph: &EGraph<TnsrLang, TnsrAnalysis>, enode: &TnsrLang) -> f32 {
     let x = |i: &Id| &egraph[*i].data;
@@ -219,7 +273,7 @@ fn get_op_cost(egraph: &EGraph<TnsrLang, TnsrAnalysis>, enode: &TnsrLang) -> f32
         | TnsrLang::Input(_)
         | TnsrLang::Noop(_) => 0.0,
 
-        TnsrLang::Ewadd([a, b]) => {
+        TnsrLang::Add([a, b]) => {
             assert!(x(a).dtype == DataKind::Tnsr);
             assert!(x(b).dtype == DataKind::Tnsr);
             if x(a).constant_foldable && x(b).constant_foldable {
@@ -259,6 +313,37 @@ fn get_op_cost(egraph: &EGraph<TnsrLang, TnsrAnalysis>, enode: &TnsrLang) -> f32
                 0.0
             } else {
                 (x(a).dims[0] * x(a).dims[1] * x(b).dims[1]) as f32 * MULTIPLY_COST
+            }
+        }
+
+        TnsrLang::Transpose([t]) => {
+            assert!(x(t).dtype == DataKind::Tnsr);
+            if x(t).constant_foldable {
+                0.0
+            } else {
+                let product: i32 = x(t).dims.iter().product::<i32>();
+                product as f32 * COPY_COST
+            }
+        }
+
+        TnsrLang::Relu([t]) => {
+            assert!(x(t).dtype == DataKind::Tnsr);
+            if x(t).constant_foldable {
+                0.0
+            } else {
+                let product: i32 = x(t).dims.iter().product::<i32>();
+                product as f32 * RELU_COST
+            }
+        }
+
+        TnsrLang::Exp([t, s]) => {
+            assert!(x(t).dtype == DataKind::Tnsr);
+            assert!(x(s).dtype == DataKind::Scalar);
+            if x(t).constant_foldable && x(s).constant_foldable {
+                0.0
+            } else {
+                let product: i32 = x(t).dims.iter().product::<i32>();
+                product as f32 * EXP_COST
             }
         }
     }
